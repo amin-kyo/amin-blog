e43031a444abb6d326a056355b8a776e9d1a06ab
amin-kyo <kyoabiz@gmail.com>
2025-08-01 00:37:25 +0900

    検索機能の実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://amin-kyo.xyz',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), pagefind()],
 });
